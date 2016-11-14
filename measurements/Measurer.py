e441fd86aa0b6a8721e3be1c92b782e4c3fa5fdd
Lucas Pradel <lucaspradel@gmail.com>
2016-11-14 14:47:43 +0100

    feat(measurements): Implemented callback for getting metrics after each end of batch

---
+++ measurements/Measurer.py
@@ -0,0 +1,23 @@
+import numpy as np
+from keras.callbacks import LambdaCallback
+
+def createBatchCallback(model, X_dev, Y_dev, X_test, Y_test, dev_scores, test_scores):
+
+    #def log_lambda_wrapper(model, X_dev, Y_dev, X_test, Y_test, dev_scores, test_scores):
+    def log_lambda(batch, logs):
+        print '\n', 'Current batch:', batch
+        print 'log: ', logs
+        printMetrics(model, X_dev, Y_dev, X_test, Y_test, dev_scores, test_scores)
+    return LambdaCallback(on_batch_end=log_lambda)
+    #return log_lambda_wrapper
+
+def printMetrics(model, X_dev, Y_dev, X_test, Y_test, dev_scores, test_scores):
+    pred_dev = model.predict(X_dev, verbose=0).argmax(axis=-1)  # Prediction of the classes
+    dev_acc = np.sum(pred_dev == Y_dev) / float(len(Y_dev))
+    dev_scores.append(dev_acc)
+    pred_test = model.predict(X_test, verbose=0).argmax(axis=-1)  # test_case_x
+    test_acc = np.sum(pred_test == Y_test) / float(len(Y_test))
+    test_scores.append(test_acc)
+
+    print "Accuracy dev: %.2f%%" % ((dev_acc * 100))
+    print "Accuracy test: %.2f%%" % ((test_acc * 100))
