2db4b043c060aa7d09fdf139305f10624f110a4c
Lucas Pradel <lucaspradel@gmail.com>
2017-02-02 12:57:51 +0100

    chore(exp1): Cleaned up and added some configuration

--- experiments/exp1.py
+++ experiments/exp1.py
@@ -1,6 +1,7 @@
 from embeddings.dependency_based_word_embeddings import DependencyBasedWordEmbeddings as Embeddings
 from datasets.conll_ner import CoNLLNer
 from datasets.wsj_pos import WSJPos
+from datasets.universal_dependencies_pos import UDPos
 from models.NER import SennaNER as NER
 from models.POS import SennaPOS as POS
 from models.Chunking import SennaChunking as Chunking
@@ -70,7 +71,7 @@ def buildAndTrainNERModel(learning_params = None):
 
     return train_scores, dev_scores, test_scores
 
-def buildAndTrainPOSModel(learning_params = None):
+def buildAndTrainWSJPOSModel(learning_params = None):
     if learning_params is None:
         params = default_params
     else:
@@ -80,6 +81,40 @@ def buildAndTrainPOSModel(learning_params = None):
     # ----- NER ----- #
 
     [input_train, train_y_cat], [input_dev, dev_y], [input_test, test_y] = WSJPos.readDataset(params['window_size'], word2Idx, case2Idx)
+    [train_x, train_case_x] = input_train
+    [dev_x, dev_case_x] = input_dev
+    [test_x, test_case_x] = input_test
+    n_out = train_y_cat.shape[1]
+
+    n_in_x = train_x.shape[1]
+    n_in_casing = train_case_x.shape[1]
+
+    # ----- Build Model ----- #
+    input_layers, inputs = InputBuilder.buildStandardModelInput(embeddings, case2Idx, n_in_x, n_in_casing, params['update_word_embeddings'])
+    model_pos = POS.buildPosModelGivenInput(input_layers, inputs, params, n_out)
+
+    print train_x.shape[0], ' train samples'
+    print train_x.shape[1], ' train dimension'
+    print test_x.shape[0], ' test samples'
+
+    # ----- Train Model ----- #
+    train_scores, dev_scores, test_scores = Trainer.trainModelWithIncreasingData(model_pos, input_train, train_y_cat, number_of_epochs,
+                                                                           params['batch_size'], input_dev,
+                                                                           dev_y, input_test, test_y,
+                                                                   measurements=[Measurer.measureAccuracy])
+
+    return train_scores, dev_scores, test_scores
+
+def buildAndTrainUDPOSModel(learning_params = None):
+    if learning_params is None:
+        params = default_params
+    else:
+        params = learning_params
+
+    word2Idx = Embeddings.word2Idx
+    # ----- NER ----- #
+
+    [input_train, train_y_cat], [input_dev, dev_y], [input_test, test_y] = UDPos.readDataset(params['window_size'], word2Idx, case2Idx)
     [train_x, train_case_x] = input_train
     [dev_x, dev_case_x] = input_dev
     [test_x, test_case_x] = input_test
@@ -140,7 +175,6 @@ def buildAndTrainChunkingModel(learning_params = None):
                                                                            dev_y, input_test, test_y,
                                                                    measurements=[biof1])
 
-
     return train_scores, dev_scores, test_scores
 
 def run_baseline_exp_with_random_params():
@@ -167,7 +201,7 @@ def run_baseline_exp_with_random_params():
                 print "Max acc test ner: %.4f in epoch: %d with samples: %d" % (score[0], score[1], sample)
                 Logger.save_reduced_datasets_results(config.experiments_log_path, 'exp_1', 'ner', 'test', params, score[0], score[1], sample)
 
-        best_train_scores_pos, best_dev_scores_pos, best_test_scores_pos = buildAndTrainPOSModel(params)
+        best_train_scores_pos, best_dev_scores_pos, best_test_scores_pos = buildAndTrainWSJPOSModel(params)
         print params
         for (sample_scores, sample) in best_train_scores_pos:
             for score in sample_scores:
@@ -214,11 +248,17 @@ def run_baseline_exp_with_fixed_params():
         print "Model nr. ", model_nr
         print fixed_params
 
-        run_build_model('ner', 'exp_1', fixed_params, buildAndTrainNERModel, 'f1', 'none')
+        if 'ner' in config.tasks:
+            run_build_model('ner', 'exp_1', fixed_params, buildAndTrainNERModel, 'f1', 'none')
 
-        run_build_model('pos', 'exp_1', fixed_params, buildAndTrainPOSModel, 'acc', 'none')
+        if 'wsj_pos' in config.tasks:
+            run_build_model('wsj_pos', 'exp_1', fixed_params, buildAndTrainWSJPOSModel, 'acc', 'none')
 
-        run_build_model('chunking', 'exp_1', fixed_params, buildAndTrainChunkingModel, 'f1', 'none')
+        if 'chunking' in config.tasks:
+            run_build_model('chunking', 'exp_1', fixed_params, buildAndTrainChunkingModel, 'f1', 'none')
+
+        if 'ud_pos' in config.tasks:
+            run_build_model('ud_pos', 'exp_1', fixed_params, buildAndTrainUDPOSModel, 'acc', 'none')
 
 def run_build_model(task, exp, params, build_model_func, score_name, transfer_models):
     train_scores, dev_scores, test_scores = build_model_func(params)
