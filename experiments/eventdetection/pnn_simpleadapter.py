edeaab25d11366478e585c6b07291bfb7e13f8c1
Lucas Pradel <lucaspradel@gmail.com>
2017-09-19 14:58:45 +0200

    feat(Scenario->orignalPNN): Added pnn original

--- experiments/eventdetection/pnn_simpleadapter.py
+++ experiments/eventdetection/pnn_simpleadapter.py
@@ -1,5 +1,6 @@
 from embeddings.dependency_based_word_embeddings import DependencyBasedWordEmbeddings as Embeddings
 from datasets.ace_ed import ACEED
+from datasets.ace_ed import LabelFilter as ACELabelFilter
 from datasets.ecbplus_ed import ECBPlusED
 from datasets.tac2015_ed import TACED
 from datasets.tempeval3_ed import TempevalED
@@ -91,6 +92,57 @@ def buildAndTrainAceModel(learning_params=None, config=[]):
         learning_params=learning_params,
         config=config)
 
+def dataset_filter_creator(label_filter):
+    def contains_labels(sentence):
+        return reduce(lambda result, word: result or label_filter(word[1]), sentence, False)
+
+    def containsOnlyOs(sentence):
+        return reduce(lambda result, word: result and word[1] == 'O', sentence, True)
+
+    def dataset_filter(dataset):
+        return filter(
+            lambda sentence: containsOnlyOs(sentence) or contains_labels(sentence),
+            dataset)
+    def dataset_wo_Os_filter(dataset):
+        return filter(
+            lambda sentence: contains_labels(sentence),
+            dataset)
+    return dataset_filter,dataset_wo_Os_filter
+
+def reader_creator(label_filter):
+    def reader(window_size, word2Idx, case2Idx):
+        dataset_filter, dataset_wo_Os_filter =  dataset_filter_creator(label_filter)
+        return ACEED.readFilteredDataset(window_size, word2Idx, case2Idx, label_filter, dataset_filter, dataset_wo_Os_filter)
+    return reader
+
+def buildAndTrainAceOnlyContactsModel(learning_params=None, config=[]):
+    return buildAndTrainPNNModel(
+        reader_creator(ACELabelFilter.only_contacts_label_filter),
+        'ace_only_contacts_',
+        learning_params=learning_params,
+        config=config)
+
+def buildAndTrainAceOnlyMovementModel(learning_params=None, config=[]):
+    return buildAndTrainPNNModel(
+        reader_creator(ACELabelFilter.only_movement_label_filter),
+        'ace_only_movement_',
+        learning_params=learning_params,
+        config=config)
+
+def buildAndTrainAceOnlyBusinessModel(learning_params=None, config=[]):
+    return buildAndTrainPNNModel(
+        reader_creator(ACELabelFilter.only_business_label_filter),
+        'ace_only_business_',
+        learning_params=learning_params,
+        config=config)
+
+def buildAndTrainAceOnlyJusticeModel(learning_params=None, config=[]):
+    return buildAndTrainPNNModel(
+        reader_creator(ACELabelFilter.only_justice_label_filter),
+        'ace_only_justice_',
+        learning_params=learning_params,
+        config=config)
+
 
 def buildAndTrainEcbModel(learning_params=None, config=[]):
     return buildAndTrainPNNModel(
@@ -141,6 +193,26 @@ def run_pnn_exp_with_fixed_params():
             runAceExp(fixed_params, ['tac'])
             runAceExp(fixed_params, ['tempeval'])
 
+        if 'ace_only_contacts' in config.tasks:
+            runAceOnlyContactsExp(fixed_params, ['ace_wo_contacts'])
+            #runAceOnlyContactsExp(fixed_params, ['tempeval'])
+            #runAceOnlyContactsExp(fixed_params, ['ecb'])
+
+        if 'ace_only_movement' in config.tasks:
+            runAceOnlyMovementExp(fixed_params, ['ace_wo_movement'])
+            #runAceOnlyMovementExp(fixed_params, ['tempeval'])
+            #runAceOnlyMovementExp(fixed_params, ['ecb'])
+
+        if 'ace_only_business' in config.tasks:
+            runAceOnlyBusinessExp(fixed_params, ['ace_wo_business'])
+            #runAceOnlyBusinessExp(fixed_params, ['tempeval'])
+            #runAceOnlyBusinessExp(fixed_params, ['ecb'])
+
+        if 'ace_only_justice' in config.tasks:
+            runAceOnlyJusticeExp(fixed_params, ['ace_wo_justice'])
+            #runAceOnlyJusticeExp(fixed_params, ['tempeval'])
+            #runAceOnlyJusticeExp(fixed_params, ['ecb'])
+
         if 'tac' in config.tasks:
             #runTacExp(fixed_params, ['ace', 'ecb', 'tempeval'])
             #runTacExp(fixed_params, ['ace', 'ecb'])
@@ -176,6 +248,42 @@ def runAceExp(params, config):
         'pnn_simple_adapter_' + adapter_size,
         params,
         buildAndTrainAceModel,
+        'f1',
+        transfer_config=config)
+
+def runAceOnlyContactsExp(params, config):
+    ExperimentHelper.run_build_model(
+        'ace_only_contacts',
+        'pnn_simple_adapter_' + adapter_size,
+        params,
+        buildAndTrainAceOnlyContactsModel,
+        'f1',
+        transfer_config=config)
+
+def runAceOnlyMovementExp(params, config):
+    ExperimentHelper.run_build_model(
+        'ace_only_movement',
+        'pnn_simple_adapter_' + adapter_size,
+        params,
+        buildAndTrainAceOnlyMovementModel,
+        'f1',
+        transfer_config=config)
+
+def runAceOnlyBusinessExp(params, config):
+    ExperimentHelper.run_build_model(
+        'ace_only_business',
+        'pnn_simple_adapter_' + adapter_size,
+        params,
+        buildAndTrainAceOnlyBusinessModel,
+        'f1',
+        transfer_config=config)
+
+def runAceOnlyJusticeExp(params, config):
+    ExperimentHelper.run_build_model(
+        'ace_only_justice',
+        'pnn_simple_adapter_' + adapter_size,
+        params,
+        buildAndTrainAceOnlyJusticeModel,
         'f1',
         transfer_config=config)
 
